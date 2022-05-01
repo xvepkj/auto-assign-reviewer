a0194ed7205bf863ce5a6df44f7ff97ca7bc3e1f
xvepkj <pkjrockzz@gmail.com>
2022-05-01 22:08:43 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,92 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 396:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 716:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(396);
+const github = __nccwpck_require__(716);
+
+
 async function run() {
-    console.log('Hello world!');
+    try {
+        const token = core.getInput("repo-access-token", { required: true });
+        const context = github.context;
+
+        if(context.payload.pullrequest == undefined)
+            throw new Error("Can't get pull request payload");
+        else {
+            const octokit = github.getOctokit(token);
+            const {assignees, number, user: { login: author }}  = context.payload.pull_request;
+            const collaborators = await octokit.request('GET /repos/{owner}/{repo}/collaborators', {
+                owner: context.repo.owner,
+                repo: context.repo.repo
+              })
+            core.info(`@${author} @${collaborators}`)
+        }        
+    }
+    catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run();
+})();
+
 module.exports = __webpack_exports__;
 /******/ })()
 ;
